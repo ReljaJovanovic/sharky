b731319faf23b47d33994d875c55550047686c5c
Relja <55156943+M1llenn1um@users.noreply.github.com>
2025-03-17 16:51:05 +0100

    place enemies

--- models/jelly-fish.class.js
+++ models/jelly-fish.class.js
@@ -1,6 +1,10 @@
 class JellyFish extends MoveableObject {
     constructor() {
         super().loadImg('assets/img/2.Enemy/2 Jelly fish/Regular damage/Lila 1.png');
+        this.x = Math.random() * 500 + 200;
+        this.height = 100;
+        this.width = 100;
+    
     }
 
 
